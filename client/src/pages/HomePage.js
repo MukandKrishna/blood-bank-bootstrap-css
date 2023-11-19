81714267208d1c2251f00eccc79a04021d777bb0
Mukand Krishna <k200409@nu.edu.pk>
2023-11-19 20:12:12 +0500

    working on landing page.
    added vector image to make attractive.

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,89 @@
-import React from "react";
-import Layout from "../components/Shared/Layout/Layout";
+import React, { useState } from "react";
+// import Layout from "../components/Shared/Layout/Layout";
+import { Link } from "react-router-dom";
 import "./HomePage.css";
 import bloodDonationImage from "./img.png";
+import Donate1 from "./Donate_1.png";
+import { FaFacebook, FaTwitter, FaInstagram, FaBars } from "react-icons/fa"; // Importing React Icons
+import { MdOutlineBloodtype } from "react-icons/md";
+
 const HomePage = () => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavVisible(!isNavVisible);
+  };
+
   return (
-    <Layout>
+    <>
+      {/* Navbar */}
+      <div className="navbar">
+        <button className="nav-toggle" onClick={toggleNav}>
+          <FaBars />
+        </button>
+        {/* Toggle Button */}
+        <ul className={`nav-links ${isNavVisible ? "active" : ""}`}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/donate">Donate Blood</Link>
+          </li>
+          <li>
+            <Link to="/register">Register Now</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </div>
+
       <h1 className="homepage">HomePage</h1>
       <div>
+        <section>
+          <div className="mission">
+            <div className="our-mission-2">
+              <div className="text-wrapper-8">Our Mission</div>
+              <p className="lorem-ipsum-is">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <div
+          className="donate-section"
+          style={{ textAlign: "center", margin: "auto" }}
+        >
+          <div className="donate-text">
+            <h1>Be a hero in someone's story - donate blood today. </h1>
+            <p>
+              Did you know that every two seconds, someone needs blood?
+              Surprisingly, only 3% of eligible people donate. Your donation
+              helps in emergencies, surgeries, and treatments for illnesses like
+              cancer. Imagine, a single car crash victim might need 100 units of
+              blood! Join the movement – over 117 million people donate each
+              year, making a huge impact. Your simple act can be a lifeline for
+              someone in need. Give the gift of life – be a blood donor and make
+              a difference!
+            </p>
+            <button className="donate_1">Donate Blood →</button>
+
+            {/* <button className="newsletter-submit-button">&gt;</button> */}
+          </div>
+          <div className="donate-image">
+            <img src={Donate1} alt="Donate Blood, Save Lives" />
+          </div>
+        </div>
+
         <section className="donation-info">
           <h2>Learn About Donation</h2>
           <p>
@@ -79,48 +156,67 @@ const HomePage = () => {
           </div>
         </section>
 
-        {/* FOOTER */}
+        {/************ FOOTER **********/}
 
-        <footer className="homepage-footer">
-          <div className="footer-column">
-            <h4>Looking for Blood</h4>
-            <ul>
-              <li>Blood Availability</li>
-              <li>Blood Bank Directory</li>
-              <li>Thalassemia Request</li>
-            </ul>
+        <footer className="footer">
+          <div className="footer-top">
+            <MdOutlineBloodtype className="footer-logo" />
+            <div className="footer-cta">
+              <span>Ready to get started?</span>
+              <button className="donate-button">Donate</button>
+            </div>
           </div>
 
-          <div className="footer-column">
-            <h4>Want to Donate Blood</h4>
-            <ul>
-              <li>Blood Donation Camps</li>
-              <li>Donor Login</li>
-              <li>About Blood Donation</li>
-              <li>Register VBD Camp</li>
-            </ul>
+          <div className="footer-middle">
+            <div className="footer-newsletter">
+              <h4>Subscribe to our newsletter</h4>
+              <div className="newsletter-input-group">
+                <input type="email" placeholder="Email address" />
+                <button type="button" className="newsletter-submit-button">
+                  &gt;
+                </button>
+              </div>
+            </div>
+            <div className="footer-links">
+              <div className="footer-column">
+                <h4>Services</h4>
+                <ul>
+                  <li>Branding</li>
+                  <li>Offline</li>
+                </ul>
+              </div>
+              <div className="footer-column">
+                <h4>About</h4>
+                <ul>
+                  <li>Our Story</li>
+                  <li>Benefits</li>
+                </ul>
+              </div>
+              <div className="footer-column">
+                <h4>Help</h4>
+                <ul>
+                  <li>FAQs</li>
+                  <li>Contact Us</li>
+                </ul>
+              </div>
+            </div>
           </div>
 
-          <div className="footer-column">
-            <h4>Blood Bank Login</h4>
-            <ul>
-              <li>DMYF Login</li>
-              <li>Add your Blood Bank</li>
-            </ul>
-          </div>
-
-          <div className="footer-column">
-            <h4>About Us</h4>
-            <ul>
-              <li>About DMYF</li>
-              <li>Notifications</li>
-              <li>DMYF FAQs</li>
-              <li>Contact Us</li>
-            </ul>
+          <div className="footer-bottom">
+            <div className="footer-bottom-links">
+              <span>Terms & Conditions</span>
+              <span>Privacy Policy</span>
+            </div>
+            <div className="footer-social">
+              <FaFacebook />
+              <FaTwitter />
+              <FaInstagram />
+            </div>
           </div>
         </footer>
       </div>
-    </Layout>
+      {/* </Layout> */}
+    </>
   );
 };
 
